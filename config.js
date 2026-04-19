30b3fd976cd6c04376cff3b1893eace9e34281ed
umutandzed <101057129+umutandzed@users.noreply.github.com>
2026-04-19 16:35:44 +0300

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-window.AUTOS_NEWS_CONFIG = window.AUTOS_NEWS_CONFIG || {
-  supabaseUrl: "https://sfmcwandhipsfyxznyeh.supabase.co",
-  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNmbWN3YW5kaGlwc2Z5eHpueWVoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY1ODczODIsImV4cCI6MjA5MjE2MzM4Mn0.O6QV1HhRkhvFOH0RpLuVhyIbPiIZ7mt6TWW_RIPeFdc",
-};
